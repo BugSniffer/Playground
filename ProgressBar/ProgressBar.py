96bd7b6c77fc779afc9d02d1aee12ddffdda449f
Rainer Menzel <rainer.menzel@web.de>
2015-02-02 19:34:35 +0100

    progress bar optimized 2nd version of progress bar added

--- ProgressBar/ProgressBar.py
+++ ProgressBar/ProgressBar.py
@@ -4,13 +4,15 @@
 import os, sys
 import time
 
+lastVal = 0
+wheelPos = 0
+
 #===============================================================================
 # Print a progress bar:
 # Input Value:
 # aktVal: integer value between 0 and 100
 #===============================================================================
-lastVal = 0
-def printProgressBar( aktVal ):
+def printProgressBar1( aktVal ):
 
     global lastVal
 
@@ -18,23 +20,55 @@ def printProgressBar( aktVal ):
         return
 
     if aktVal != lastVal:
-        outStr = '[%s] %3d%% \r' % ( ( " " * 100 ), aktVal )
-        sys.stdout.write( outStr )
-        outStr = '[' + '#' * aktVal + '\r'
-        sys.stdout.write( outStr )
-        sys.stdout.flush()
         lastVal = aktVal
 
+    outStr = '[%s%s] %3d%% \r' % ( ( "#" * aktVal ), ( " " * ( 100 - aktVal ) ), aktVal )
+    sys.stdout.write( outStr )
+    sys.stdout.flush()
+    lastVal = aktVal
+
+#===============================================================================
+# Print a progress bar:
+# Input Value:
+# aktVal: integer value between 0 and 100
+#===============================================================================
+def printProgressBar2( aktVal ):
+
+    wheelChar = ['|', '/', '-', '\\']
+
+    global lastVal
+    global wheelPos
+
+    if aktVal < 0 or aktVal > 100:
+        return
+
+    if aktVal != lastVal:
+        lastVal = aktVal
+        wheelPos = 0
+
+    if aktVal < 100:
+        outStr = '[%s%s%s] %3d%% \r' % ( ( "#" * aktVal ), wheelChar[wheelPos], ( " " * ( 100 - aktVal - 1 ) ), aktVal )
+    else:
+        outStr = '[%s] %3d%% \r' % ( ( "#" * aktVal ), aktVal )
+    sys.stdout.write( outStr )
+    sys.stdout.flush()
+    lastVal = aktVal
+    if wheelPos >= 3:
+        wheelPos = 0
+    else:
+        wheelPos = wheelPos + 1
 
 
 def main():
 
     ii = 0
-    while ii < 100:
+    startTime = time.time()
+    while ii <= 1000:
+        printProgressBar2( ii / 10 )
         ii = ii + 1
-        time.sleep( 0.4 )
-        printProgressBar( ii )
+#         time.sleep( 0.2 )
     print
+    print ( time.time() - startTime )
 
 
 
