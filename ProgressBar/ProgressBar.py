3c30311b5cc6a3f964869a1fa28b8e782054248f
Rainer Menzel <rainer.menzel@web.de>
2015-02-04 10:25:43 +0100

    2nd variant of ProgressBar added
    ADD: type #2 of ProgressBar that prints an additional "wheel char"
    ADD: read type of PB, sleep time and start value from concole input for testing

--- ProgressBar/ProgressBar.py
+++ ProgressBar/ProgressBar.py
@@ -8,11 +8,19 @@ import time
 #===============================================================================
 # Class ProgressBar:
 #===============================================================================
+#===============================================================================
+# Print a progress bar:
+# Input Value:
+# aktVal: integer value between 0 and 100
+#===============================================================================
 class ProgressBar:
+    wheelChar = ['|', '/', '-', '\\']
 
-    def __init__( self ):
+    def __init__( self, barType ):
         self.lastVal = 0
+        self.barType = barType
         self.wheelPos = 0
+#         print 'Type = %d' % self.barType
 
     def printOut( self, percent ):
         if percent < 0 or percent > 100:
@@ -20,95 +28,47 @@ class ProgressBar:
     
         if percent != self.lastVal:
             self.lastVal = percent
+            self.wheelPos = 0
+        if self.barType == 1:
+            outStr = '[%s%s] %3d%% \r' % ( ( "#" * percent ), ( " " * ( 100 - percent ) ), percent )
     
-        outStr = '[%s%s] %3d%% \r' % ( ( "#" * percent ), ( " " * ( 100 - percent ) ), percent )
+        if self.barType == 2:
+            if percent < 100:
+                outStr = '[%s%s%s] %3d%% \r' % ( ( "#" * percent ), ProgressBar.wheelChar[self.wheelPos], ( " " * ( 100 - percent - 1 ) ), percent )
+            else:
+                outStr = '[%s] %3d%% \r' % ( ( "#" * percent ), percent )
+            if self.wheelPos >= 3:
+                self.wheelPos = 0
+            else:
+                self.wheelPos += 1
+
         sys.stdout.write( outStr )
         sys.stdout.flush()
         self.lastVal = percent
 
 
+#===============================================================================
+# main
+#===============================================================================
 def main():
- 
-    ii = 0
-    pb = ProgressBar()
+
+    pbType = int( raw_input( 'Typ (1=normal 2=mit Zwischenschritten):' ) ) 
+    sleepTime = float( raw_input( 'Wartezeit [sec.]:' ) )
+    startVal = int( raw_input( 'Startwert [%]:' ) )
+
+    ii = startVal * 10
+    pb = ProgressBar( pbType )
 
     startTime = time.time()
     while ii <= 1000:
         pb.printOut( ii / 10 )
         ii = ii + 1
-#         time.sleep( 0.02 )
+        time.sleep( sleepTime )
     print
     print ( time.time() - startTime )
 
 
-
-# lastVal = 0
-# wheelPos = 0
-# 
-# #===============================================================================
-# # Print a progress bar:
-# # Input Value:
-# # aktVal: integer value between 0 and 100
-# #===============================================================================
-# def printProgressBar1( aktVal ):
-# 
-#     global lastVal
-# 
-#     if aktVal < 0 or aktVal > 100:
-#         return
-# 
-#     if aktVal != lastVal:
-#         lastVal = aktVal
-# 
-#     outStr = '[%s%s] %3d%% \r' % ( ( "#" * aktVal ), ( " " * ( 100 - aktVal ) ), aktVal )
-#     sys.stdout.write( outStr )
-#     sys.stdout.flush()
-#     lastVal = aktVal
-# 
-# #===============================================================================
-# # Print a progress bar:
-# # Input Value:
-# # aktVal: integer value between 0 and 100
-# #===============================================================================
-# def printProgressBar2( aktVal ):
-# 
-#     wheelChar = ['|', '/', '-', '\\']
-# 
-#     global lastVal
-#     global wheelPos
-# 
-#     if aktVal < 0 or aktVal > 100:
-#         return
-# 
-#     if aktVal != lastVal:
-#         lastVal = aktVal
-#         wheelPos = 0
-# 
-#     if aktVal < 100:
-#         outStr = '[%s%s%s] %3d%% \r' % ( ( "#" * aktVal ), wheelChar[wheelPos], ( " " * ( 100 - aktVal - 1 ) ), aktVal )
-#     else:
-#         outStr = '[%s] %3d%% \r' % ( ( "#" * aktVal ), aktVal )
-#     sys.stdout.write( outStr )
-#     sys.stdout.flush()
-#     lastVal = aktVal
-#     if wheelPos >= 3:
-#         wheelPos = 0
-#     else:
-#         wheelPos = wheelPos + 1
-
-
-# def main():
-# 
-#     ii = 0
-#     startTime = time.time()
-#     while ii <= 1000:
-#         printProgressBar2( ii / 10 )
-#         ii = ii + 1
-# #         time.sleep( 0.2 )
-#     print
-#     print ( time.time() - startTime )
-
-
+#===============================================================================
 
 if __name__ == "__main__":
     main()
