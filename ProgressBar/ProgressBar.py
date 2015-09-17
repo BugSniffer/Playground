54673012c3fa86c126de012e4173695e4ea94c80
Rainer Menzel <rainer.menzel@web.de>
2015-09-17 17:16:21 +0200

    set color codes via init method and some optimisation

--- ProgressBar/ProgressBar.py
+++ ProgressBar/ProgressBar.py
@@ -13,11 +13,13 @@ class ProgressBar:
 #===============================================================================
     wheelChar = ['|', '/', '-', '\\']
 
-    def __init__( self, capture, char, style = 0, min = 0, max = 100, unit = '%' ):
+    def __init__( self, capture, char, style = 0, colorF = '\033[7;49m', colorB = '\033[1;30m', min = 0, max = 100, unit = '%' ):
 #===============================================================================
 # Parameters:
 # capture - String printed in front of the progress bar
 # char    - String containing a character to be used for the progress bar
+#           '#' is the default for style 0 and 1
+#           ' ' is the default for style 2 (will be displayed inverse)
 # style   - The following styles can be selected by creating an object:
 #           0 - simple progress bar with single percentage step:
 #               [#######                    ] 37%
@@ -25,9 +27,18 @@ class ProgressBar:
 #               dash) at the current position indicating activity between two
 #               percentage steps 
 #               [#######-                   ] 37%
-# min     - minimum value of the possible value range
-# max     - maximum value of the possible value range
-# unit    - unit of the values
+#           2 - solid progress bar without [...] at the beginning and end of the
+#               bar
+# colorF  - string containing escape sequence to switch to specific foreground 
+# colorB    and background color of the progress bar if style 2 is selected.
+#           Default values are:
+#           colorF = '\033[7;49m' (inverse standard terminal color)
+#           colorB = '\033[1;30m' (grey)
+# min     - minimum/maximum value of the possible value range
+# max       Default values are:
+#           min = 0
+#           max = 100
+# unit    - string containing the unit of the values. Default string is '%'
 #===============================================================================
         if capture == None:
             self.__capture = ''
@@ -35,9 +46,7 @@ class ProgressBar:
             self.__capture = capture
 
         self.__char = char
-        if style == 0 and char == None:
-            self.__char = '#'
-        if style == 1 and char == None:
+        if style <= 1 and char == None:
             self.__char = '#'
         if style == 2 and char == None:
             self.__char = ' '
@@ -50,10 +59,17 @@ class ProgressBar:
         self.__unit = unit
         self.__scale = float( self.__maxVal - self.__minVal ) / 100.0
 
+        if self.__style == 2:
+            self.__colorF = colorF  # set foreground color
+            self.__colorB = colorB  # set background color
+            self.__colorRst = '\033[0m'  # reset colors to default
+
+
+
     def printOut( self, actVal ):
 #===============================================================================
 # Print the progress bar:
-# actVal: integer value between 0 and 100
+# actVal: integer value in the given range between min and max
 #===============================================================================
         if actVal < self.__minVal or actVal > self.__maxVal:
             return
@@ -75,11 +91,7 @@ class ProgressBar:
             self.__wheelPos %= len( ProgressBar.wheelChar )
 
         if self.__style == 2:
-            colBar = '\033[7;49m'  # standard terminal color
-            colNoBar = '\033[1;30m'  # grey
-            colReset = '\033[0m'
-
-            outStr = '%s%s%s%s%s%s%3d%s   \r' % ( self.__capture, colBar, ( self.__char * percent ), colNoBar, ( " " * ( 100 - percent ) ), colReset, actVal, self.__unit )
+            outStr = '%s%s%s%s%s%s%3d%s   \r' % ( self.__capture, self.__colorF, ( self.__char * percent ), self.__colorB, ( " " * ( 100 - percent ) ), self.__colorRst, actVal, self.__unit )
     
         sys.stdout.write( outStr )
         sys.stdout.flush()
